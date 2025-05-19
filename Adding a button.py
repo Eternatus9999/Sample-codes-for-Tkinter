dd7bbb55d15d521ebb39c464c41af427ab753eef
Eternatus9999 <eternatus9999@gmail.com>
2025-05-19 19:42:12 +0530

    add disable function and inserting image methods to the code to study

--- Adding a button.py
+++ Adding a button.py
@@ -13,6 +13,14 @@ def clicked():
     lbl.configure(text="Button Clicked")
 
 btn = Button(root, text="Click Me", fg = "red", bg= "yellow", command=clicked, activebackground="lime", activeforeground="blue")
+btn.config(state=ACTIVE) # to enable the button(ACTIVE/DISABLED)
+
+# To add Image to a button
+
+# image = PhotoImage(file="button.png")
+# btn.config(image=image)
+# btn.config(compound=LEFT)
+
 btn.grid(column=1, row=0)
 
 root.mainloop()
