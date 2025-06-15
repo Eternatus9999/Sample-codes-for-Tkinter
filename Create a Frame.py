5e564b41bb240cce426cb91ea5f8bbde6c83d92f
Eternatus9999 <eternatus9999@gmail.com>
2025-06-15 17:22:58 +0530

    Initial commit

--- Create a Frame.py
+++ Create a Frame.py
@@ -4,6 +4,12 @@ root = Tk()
 
 root.title("Printer")
 
-root.geometry("1920x1800")
+root.geometry("400x400")
+
+frame = Frame(root, bg="black")
+frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+button_frame = Frame(frame, bg="red")
+button_frame.pack(side=TOP)
 
 root.mainloop()
